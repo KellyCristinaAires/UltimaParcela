6d34b51ba25d4548b22dda9aea2fc9650dd1cb3a
KellyCristinaAires <k.woys@hotmail.com>
2020-10-10 17:10:46 -0300

    atividade ultima parcela

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace ultimaparcela
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double valorTotal;
+            double parcelas;
+            double result;
+
+            DateTime dataAtual = DateTime.Now;
+            DateTime parcelaFinal;
+
+            Console.Write("Digite o valor Total: ");
+            valorTotal = double.Parse(Console.ReadLine() );
+            
+            Console.Write("Digite o valor de Parcelas: ");
+            parcelas = double.Parse(Console.ReadLine());
+            Console.Write("\n");
+
+            result = valorTotal / parcelas;
+
+            parcelaFinal = dataAtual.AddMonths(8);
+            Console.WriteLine($"Sua ultima parcela será em: {parcelaFinal},no valor de R${result}");
+        }
+    }
+}
